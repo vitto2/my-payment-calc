3453ba2013d1a1561b6e1d6a3ac6d1437042bfa4
vitto2 <vitor2071@gmail.com>
2022-10-16 20:14:29 -0300

    criando propriedade para armazenar valor do salario

--- funcionario.dart
+++ funcionario.dart
@@ -1,8 +1,10 @@
 mixin Funcionario {
   double? _timeWorked;
+  double? payment;
 
   getTimeWorked() => this._timeWorked;
   setTimeWorked(double value) => this._timeWorked = value;
 
-  paymant_calc(double valueHoursWorked) => valueHoursWorked * getTimeWorked();
+  paymant_calc(double valueHoursWorked) =>
+      this.payment = valueHoursWorked * getTimeWorked();
 }
